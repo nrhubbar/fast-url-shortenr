e3b160336f92b3bf9c1643b99e2df96fb2f0da15
Nicholas Hubbard 👽 <me@nicholas-hubbard.com>
2022-02-15 00:57:36 -0800

    Implement better hash collision and duplicate catching

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from wsgiref.util import request_uri
 from fastapi import FastAPI
 from fastapi.responses import FileResponse,RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -5,7 +6,10 @@ from pydantic import BaseModel
 from decouple import config
 import hashlib
 import base64
+import time
 import redis
+
+HASH_LENGTH=int(config("HASH_LENGTH"))
 
 class CreateShortUrlRequest(BaseModel):
     url: str
@@ -24,18 +28,21 @@ async def index():
 
 @app.post("/")
 async def create_short_url(request: CreateShortUrlRequest):
-    url_to_shorten = request.url.encode('utf-8')
-    url_hash_bytes = hashlib.sha1(url_to_shorten).digest()
-    url_safe_hash = base64.urlsafe_b64encode(url_hash_bytes)
-    url_safe_str = url_safe_hash.decode("utf-8")
 
-    slice_index = 5
-    shortened_url_key = url_safe_str[0:slice_index]
+    shortened_url_key = get_key_from_url(request.url)
     saved_url = redis_client.setnx(name=shortened_url_key, value=request.url)
-    while saved_url == 0:
-        slice_index += 1
-        shortened_url_key = url_safe_str[0:slice_index]
-        saved_url = redis_client.setnx(name=shortened_url_key, value=request.url)
+    
+    if saved_url == 0:
+        if (redis_client.get(request.url) != None):
+            return {
+                "shortUrl": shortened_url_key
+            }
+
+        while saved_url == 0:
+            shortened_url_key = get_key_from_url(request_uri, time.time_ns())
+            saved_url = redis_client.setnx(name=shortened_url_key, value=request.url)
+    
+    redis_client.set(request.url, shortened_url_key)
     
     return {
         "shortUrl": shortened_url_key
@@ -51,4 +58,9 @@ async def get_short_url(short_id: str):
     
     return RedirectResponse(long_url.decode("utf-8"))
 
-
+def get_key_from_url(url: str, salt=""):
+    url_to_shorten = f'{url}{salt}'.encode('utf-8')
+    url_hash_bytes = hashlib.sha1(url_to_shorten).digest()
+    url_safe_hash = base64.urlsafe_b64encode(url_hash_bytes)
+    url_safe_str = url_safe_hash.decode("utf-8")
+    return url_safe_str[0:HASH_LENGTH]
